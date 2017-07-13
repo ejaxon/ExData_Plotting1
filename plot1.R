51bc746fd39f567f78bb98098fe50786f7bc4e8a
ejaxon <eric@deepweave.com>
2017-07-13 07:37:12 -0400

    Added script for plot 1.

---
+++ plot1.R
@@ -0,0 +1,17 @@
+# Set the time period of interest
+start <- as.Date("2007-01-31")
+end <- as.Date("2007-02-03")
+
+# Read the data
+indata <- read.table(file="household_power_consumption.txt", sep=";", na.strings="?", header=TRUE)
+# Extract rows from time period of interest, convert Date column to Date and Date/Time columns to DateTime
+indata$Date <- as.Date(indata$Date, "%d/%m/%Y")
+data <- subset(indata, (indata$Date > start & indata$Date < end))
+data$DateTime <- strptime(paste(data$Date, data$Time, " "), "%Y-%m-%d %H:%M:%S")
+hist(data$Global_active_power, main="Global Active Power", ylab="Frequency", xlab="Global Active Power (kilowatts)", col="red", axes=FALSE)
+# Draw the axes with magnification .7 so that all labels show even in a smaller plot
+axis(side=2, at=c(0, 200, 400, 600, 800, 1000, 1200), labels=TRUE, cex.axis=.7)
+axis(side=1,labels=TRUE, cex.axis=.7)
+
+
+
